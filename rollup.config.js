4a21268e91e00b797dd993682184709d1375538c
chekofif <gidi@gidi.io>
2017-06-08 09:29:28 +0100

    fixed import of lodash which fixes e2e tests

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,27 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import closure from 'rollup-plugin-closure-compiler-js';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'modules/index.js',
   format: 'iife',
   plugins: [
-    resolve(),
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: 'node_modules/**',  // Default: undefined
+
+      // if true then uses of `global` won't be dealt with by this plugin
+      ignoreGlobal: false,  // Default: false
+
+      // if false then skip sourceMap generation for CommonJS modules
+      sourceMap: false  // Default: true
+    }),
     babel({
       babelrc: false,
       "presets": [
@@ -20,8 +35,7 @@ export default {
       ],
       "plugins": [
         "external-helpers"
-      ],
-      exclude: 'node_modules/**' // only transpile our source code
+      ]
     }),
     closure()
   ],
