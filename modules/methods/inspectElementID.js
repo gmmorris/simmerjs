5f94b60883c1aef15ee150ec8e63d8a885a9ffed
chekofif <gidi@gidi.io>
2017-06-27 01:25:05 +0100

    cleaned up element ID inspection method

--- modules/methods/inspectElementID.js
+++ modules/methods/inspectElementID.js
@@ -6,36 +6,35 @@ import { attr } from './validationHelpers'
  * @param {object} state. The current selector state (has the stack and specificity sum)
  */
 export default function (hierarchy, state, validateSelector, config, query) {
-  var index, currentElem, currentID
-  for (index = 0; index < hierarchy.length && !state.verified; index += 1) {
-    currentElem = hierarchy[index]
-    currentID = attr(currentElem.el.getAttribute('id'))
-    // make sure the ID is unique
-    if (currentID && isUniqueElementID(query, currentID)) {
-      state.stack[index].push("[id='" + currentID + "']")
-      state.specificity += 100
+  return hierarchy.reduce((selectorState, currentElem, index) => {
+    if (!selectorState.verified) {
+      const [validatedState] = [currentElem.el.getAttribute('id')]
+        .filter(id => attr(id))
+        // make sure the ID is unique
+        .filter(id => isUniqueElementID(query, id))
+        .map(validId => {
+          selectorState.stack[index].push(`[id='${validId}']`)
+          selectorState.specificity += 100
 
-      // if the index is 0 then this is the ID of the actual element! Which means we have found our selector!
-      if (index === 0) {
-        // An ID provides the highest specificity so we start by verifying the query's success so that, maybe, this will be enough
-        // Note that the first element in the hierarchy (index 0) is the actual element we are looking to parse
-        if (validateSelector(state)) {
-          // The ID worked like a charm - mark this state as verified and move on!
-          state.verified = true
-        } else {
-          // The ID wasn't enough, this means the page, this should never happen as we tested for the ID's uniquness, but just incase
-          // we will pop it from the stack as it only adds noise
-          state.stack[index].pop()
-          state.specificity -= 100
-        }
-      } else if (state.specificity >= config.specificityThreshold) {
-        // we have reached the minimum specificity, lets try verifying now, as this will save us having to add more IDs to the selector
-        if (validateSelector(state)) {
-          // The ID worked like a charm - mark this state as verified and move on!
-          state.verified = true
-        }
-      }
+          if (selectorState.specificity >= config.specificityThreshold) {
+            // we have reached the minimum specificity, lets try verifying now, as this will save us having to add more IDs to the selector
+            if (validateSelector(selectorState)) {
+              // The ID worked like a charm - mark this state as verified and move on!
+              selectorState.verified = true
+            }
+          }
+
+          if (!selectorState.verified && index === 0) {
+            // if the index is 0 then this is the ID of the actual element! Which means we have found our selector!
+            // The ID wasn't enough, this means the page, this should never happen as we tested for the ID's uniquness, but just incase
+            // we will pop it from the stack as it only adds noise
+            selectorState.stack[index].pop()
+            selectorState.specificity -= 100
+          }
+          return selectorState
+        })
+      return validatedState || selectorState
     }
-  }
-  return state
+    return selectorState
+  }, state)
 }
